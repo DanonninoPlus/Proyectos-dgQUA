5d399fba25358a20408e5844aec5fea1658e911f
DanonninoPlus <ivan.risueno@gmail.com>
2026-01-09 10:44:40 -0600

    RollBack y actualización para EUA

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const projNotas = document.getElementById("projNotas");
 let proyectos = [];
 let permisos = [];
 
-const PAISES_CON_SUBTIPO = ["Japón", "Chile", "Canadá"];
+const PAISES_CON_SUBTIPO = ["Japón", "Chile", "Estados Unidos"];
 const CAMPO_SUBTIPO = "Tipo de proyecto";
 
 
@@ -253,7 +253,7 @@ paisHeader.innerHTML = `📍 ${pais} <span class="text-sm text-gray-500 ml-2">($
       paisDiv.appendChild(paisContent);
 
 
-// 🟣 Países con subniveles (Japón, Chile, Canadá)
+// 🟣 Países con subniveles (Japón, Chile, Estados Unidos)
 if (PAISES_CON_SUBTIPO.includes(pais) && typeof dataPais === "object" && !Array.isArray(dataPais)) {
 
 
@@ -825,3 +825,4 @@ function populateResponsibles() {
     filterResponsible.appendChild(opt);
   });
 }
+
