2bac3ce372ff6abda55287474c1914f35e1ecd5a
DanonninoPlus <ivan.risueno@gmail.com>
2026-03-25 11:22:51 -0600

    Update capacitacion.js

--- capacitacion.js
+++ capacitacion.js
@@ -1,3 +1,30 @@
+
+//LÍNEA PARA AGREGAR EL ARREGLO DE INSTITUCIONES
+
+function renderLista(lista, contenedorId){
+
+const contenedor = document.getElementById(contenedorId);
+
+if(!lista || !Array.isArray(lista)) return;
+
+lista.forEach(item => {
+
+const li = document.createElement("li");
+
+li.className = "flex items-center gap-2";
+
+li.innerHTML = `
+<span class="w-1 h-1 bg-primary rounded-full"></span>
+${item}
+`;
+
+contenedor.appendChild(li);
+
+});
+
+}
+
+//LINEAS PARA LLENAR EL CONTENIDO EN EL HTML
 async function cargarCapacitacion() {
 
   const params = new URLSearchParams(window.location.search);
@@ -36,12 +63,25 @@ async function cargarCapacitacion() {
   document.getElementById("findifusionCapacitacion").textContent = capacitacionEncontrada.findifusion || "---";
   document.getElementById("dtedifusionCapacitacion").textContent = capacitacionEncontrada.dtedifusion || "---";
   document.getElementById("vigenciaCapacitacion").textContent = capacitacionEncontrada.vigencia || "---";
-  document.getElementById("institucionesdifCapacitacion").textContent = capacitacionEncontrada.institucionesdif || "---";
   document.getElementById("dtepromoCapacitacion").textContent = capacitacionEncontrada.dtepromo || "---";
-  document.getElementById("institucionespartCapacitacion").textContent = capacitacionEncontrada.institucionespart || "---";
   document.getElementById("dteresultadosCapacitacion").textContent = capacitacionEncontrada.dteresultados || "---";
   document.getElementById("aceptadosCapacitacion").textContent = capacitacionEncontrada.aceptados || "---";
   document.getElementById("rechazdosCapacitacion").textContent = capacitacionEncontrada.rechazdos || "---";
+
+//Solo para Institutiones por generar un arreglo en el .json
+
+    // document.getElementById("institucionesdifCapacitacion").textContent = capacitacionEncontrada.institucionesdif || "---";
+    // document.getElementById("institucionespartCapacitacion").textContent = capacitacionEncontrada.institucionespart || "---";
+
+renderLista(
+capacitacionEncontrada.institucionesdif,
+"institucionesdifCapacitacion"
+);
+
+renderLista(
+capacitacionEncontrada.institucionespart,
+"institucionespartCapacitacion"
+);
 
 
 }
